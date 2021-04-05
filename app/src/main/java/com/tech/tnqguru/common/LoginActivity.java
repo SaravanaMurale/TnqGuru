42ea049480390f174d0ef75c6c19968fb4f533ab
Krishna <mathi3567>
2021-04-05 11:30:01 +0530

    Tnq guru update

--- app/src/main/java/com/tech/tnqguru/common/LoginActivity.java
+++ app/src/main/java/com/tech/tnqguru/common/LoginActivity.java
@@ -54,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,RegistrationActivity.class);
+//                Intent intent=new Intent(LoginActivity.this,CollegeFacRegFragment.class);
                 startActivity(intent);
             }
         });
