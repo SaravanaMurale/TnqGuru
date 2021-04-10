68b87b6029441fd2a1487be73447084a89a2ca9d
murali <murali.e@sosaley.in>
2021-04-10 18:14:50 +0530

    School Fac Reg UI Commit

--- app/src/main/java/com/tech/tnqguru/common/ColgStuRegFragment.java
+++ app/src/main/java/com/tech/tnqguru/common/ColgStuRegFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,11 +14,27 @@ import com.tech.tnqguru.R;
 
 public class ColgStuRegFragment extends Fragment {
 
+    Spinner spinnerSelectScholStu,spinnerScholStuModeOfClass;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.layout_colg_stu_reg,container,false);
 
+        initView(view);
+
         return view;
+    }
+
+    private void initView(View view) {
+
+        setView(view);
+
+    }
+
+    private void setView(View view) {
+
+        spinnerSelectScholStu = (Spinner) view.findViewById(R.id.spinnerSelectColg);
+        spinnerScholStuModeOfClass = (Spinner) view.findViewById(R.id.spinnerColgFacCountry);
     }
 }
