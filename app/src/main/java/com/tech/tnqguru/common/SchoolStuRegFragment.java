7c677372ca846fe21b04b86293cdd9fdcafd7bd7
murali <murali.e@sosaley.in>
2021-05-06 13:01:22 +0530

    School Fac Reg UI Commit

--- app/src/main/java/com/tech/tnqguru/common/SchoolStuRegFragment.java
+++ app/src/main/java/com/tech/tnqguru/common/SchoolStuRegFragment.java
@@ -1,27 +1,46 @@
 package com.tech.tnqguru.common;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.tech.tnqguru.R;
+import com.tech.tnqguru.utils.MathUtil;
 
-public class SchoolStuRegFragment  extends Fragment {
+import java.util.Calendar;
+import java.util.Date;
+
+public class SchoolStuRegFragment  extends Fragment implements AdapterView.OnItemSelectedListener {
 
 
     Spinner spinnerScholStuReg,spinnerScholStuModeOfClass,spinnerScholStuCountry;
-    String spnSelectScholStu,spnScholStuModeOfClass;
+    String spnScholStuSelectSchol,spnScholStuSelectModeOfClass;
     Button btnScholStuReg;
 
     EditText scholStuNameEdit,scholStuMobileEdit,scholStuAddressEdit,scholStuPincodeEdit,scholStuEmailEdit,scholStuIdProofNumberEdit,scholStuPasswordEdit;
+
+    TextView scholStuDOB;
+    RelativeLayout stuDOBBlock;
+    String scholStuDOBInString;
+
+    DatePickerDialog.OnDateSetListener setListenerDateOfBirth;
+
 
     @Nullable
     @Override
@@ -65,13 +84,56 @@ public class SchoolStuRegFragment  extends Fragment {
 
         setView(view);
 
+
+        ArrayAdapter<CharSequence> selectSchool=ArrayAdapter.createFromResource(getActivity(),R.array.select_school,android.R.layout.simple_spinner_item);
+        selectSchool.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerScholStuReg.setAdapter(selectSchool);
+        spinnerScholStuReg.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> selectSchoolModeOfClass=ArrayAdapter.createFromResource(getActivity(),R.array.mode_class,android.R.layout.simple_spinner_item);
+        selectSchoolModeOfClass.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerScholStuModeOfClass.setAdapter(selectSchoolModeOfClass);
+        spinnerScholStuModeOfClass.setOnItemSelectedListener(this);
+
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int date = calendar.get(Calendar.DATE);
+
+        stuDOBBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), android.R.style.Theme_Holo_Light_Dialog, setListenerDateOfBirth, year, month, date);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+
+            }
+        });
+
+        setListenerDateOfBirth = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                scholStuDOB.setText(date);
+
+                scholStuDOBInString=date;
+
+                System.out.println("selectedFromDate " + date);
+
+
+            }
+        };
+
     }
 
     private void setView(View view) {
 
         spinnerScholStuReg = (Spinner) view.findViewById(R.id.spinnerSelectSchool);
-        spinnerScholStuModeOfClass = (Spinner) view.findViewById(R.id.spinnerScholStuCountry);
-        spinnerScholStuCountry = (Spinner) view.findViewById(R.id.modeOfScholStuClass);
+        spinnerScholStuModeOfClass = (Spinner) view.findViewById(R.id.modeOfScholStuClass);
+
 
         scholStuNameEdit=(EditText)view.findViewById(R.id.scholStuName);
         scholStuMobileEdit=(EditText)view.findViewById(R.id.scholStuMobile);
@@ -81,7 +143,31 @@ public class SchoolStuRegFragment  extends Fragment {
         scholStuPasswordEdit= (EditText)view.findViewById(R.id.scholStuPassword);
         //scholStuIdProofNumberEdit=(EditText)view.findViewById(R.id.colgStuPassword);
 
+        stuDOBBlock=(RelativeLayout)view.findViewById(R.id.stuDOBBlock);
+        scholStuDOB=(TextView)view.findViewById(R.id.scholStuDOB);
+
         btnScholStuReg=(Button) view.findViewById(R.id.btnScholStuReg);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        if (adapterView.getId() == R.id.spinnerSelectSchool) {
+
+            spnScholStuSelectSchol = adapterView.getItemAtPosition(i).toString();
+            System.out.println("ColgUGorPG " + spnScholStuSelectSchol);
+
+        } else if (adapterView.getId() == R.id.spinnerColgFacCountry) {
+
+            spnScholStuSelectModeOfClass = adapterView.getItemAtPosition(i).toString();
+            System.out.println("countrySelected " + spnScholStuSelectModeOfClass);
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
