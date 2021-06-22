d765b717f4379e795201d4c3448ed997272bd877
Mathivanan <mmathivanan17@gmail.com>
2021-06-22 21:18:10 +0530

    Dummy Updated

---
+++ app/src/main/java/com/tech/tnqguru/DummyActivity.java
@@ -0,0 +1,13 @@
+package com.tech.tnqguru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
